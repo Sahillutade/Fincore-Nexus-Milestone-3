df57cef55c7252b5aa51d8ed36bfdba3f185650d
Sahillutade <lutadesahil@gmail.com>
2026-08-05 18:07:01 +0530

    WIP: Payment Service

---
+++ backend/paymentservice/src/main/java/com/infosys/finbank/payment/PaymentserviceApplication.java
@@ -0,0 +1,16 @@
+package com.infosys.finbank.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@EnableDiscoveryClient
+@EnableFeignClients
+public class PaymentserviceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentserviceApplication.class, args);
+    }
+}
